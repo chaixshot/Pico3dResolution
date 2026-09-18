690094e7c1bb36d57a79c16bf2971336420a5760
chaixshot <chai5142xshot@gmail.com>
2026-09-18 16:11:05 +0700

    pump version 1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.hamer.res3d"
         minSdk = 29
         targetSdk = 37
-        versionCode = 12
-        versionName = "1.2"
+        versionCode = 13
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
